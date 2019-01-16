3954abbf27510ce080f5431869d778cd2a415c77
CorvetteCole <corvettecole@gmail.com>
2019-01-16 18:27:07 -0500

    add lab 1

--- Lab1/Project1/Source.cpp
+++ Lab1/Project1/Source.cpp
@@ -14,30 +14,26 @@ int main() {
 	cout << "   By Cole Gerdemann      \n";
 	cout << "\n";
 
-	//obtain two numbers from the user
 	int numberOne;
 	int numberTwo;
 	cout << "Enter a number: ";
 	cin >> numberOne;
 	cout << "Enter another: ";
 	cin >> numberTwo;
-
 	cout << "\n";
-
-	//math block using the two numbers obtained from the user
 	int sum = numberOne + numberTwo;
 	int difference = numberOne - numberTwo;
 	int product = numberOne * numberTwo;
 	int lossyQuotient = numberOne / numberTwo;
 	float quotient = (float)numberOne / numberTwo;
 
-	//display math results using the two numbers
 	cout << setw(21) << left << "Sum: " << sum << "\n";
 	cout << setw(21) << left << "Difference: " << difference << "\n";
 	cout << setw(21) << left << "Product: " << product << "\n";
 	cout << setw(21) << left << "Int Quotient: " << lossyQuotient << "\n";
 	cout << setw(21) << left << "Quotient: " << quotient << "\n";
 	
+
 	system("pause");
 	return 0;
 
