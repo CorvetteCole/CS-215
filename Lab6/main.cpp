af82390e4561504a16b12555800b03b368c69492
Cole Gerdemann <corvettecole@gmail.com>
2019-02-27 15:45:36 -0500

    finished lab6

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,12 +1,21 @@
+//--------------------------------------------------------------------
+// CS215-005 LAB 6
+//--------------------------------------------------------------------
+// Author: Cole Gerdemann
+// Date: February 27th 2019
+// Description: Program that analyzes the daily prices of a corporate
+// stock.
+//--------------------------------------------------------------------
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <string>
 #include <array>
 
-
 using namespace std;
 
+// there are no limitations on these settings, program can handle almost anything
 const int timeStart = 800;      // 24 hour time of 8am
 const int timeIncrement = 60;  // default 1 hour between prices recorded
 const int numDataPoints = 10;   // number of stock price data points
@@ -26,33 +35,67 @@ string get12HourTime(int time){
     }
     return to_string(hour) + ":" + minuteString;
 }
-
+// robust time incrementing solution allowing for any timeIncrement value
 void incrementTime(int &time){
-    time += timeIncrement;
+    if (timeIncrement >= 60){
+        time += (timeIncrement / 60) * 100;
+        time += timeIncrement - (timeIncrement / 60) * 60;
+    } else {
+        time += timeIncrement;
+    }
+
     int hour = time / 100;
     int minute = (int)(round(((time / 100.0) - hour) * 100));
     if (minute >= 60){
         time -= 60;  // subtract the 60 minutes
         time += 100; // add one hour
     }
+
+    if (time >= 2500){
+        time -= 2400;
+    }
 }
 
-// #TODO see if I can combine the incrementTime and get12HourTime functions in a readable way
 void printStockInfo(const string &ticker, const string &date, array<double, numDataPoints> price){
     int time = timeStart;
+    double change;
+    double high = DBL_MIN;
+    double low = DBL_MAX;
+    double average;
+    double total = 0;
+    double median;
+
     cout << "STOCK: " << setw(6) << left << ticker;
     cout << "DATE: " << setw(10) << left << date << endl;
     cout << setw(12) << left << "TIME";
     cout << setw(15) << left << "PRICE" << endl;
 
+    // print prices with time and also do stuff needed for future calculations
     for (double i : price){
-        //cout << time << endl;
         cout << setw(5) << right << get12HourTime(time);
         cout << "       $" << setw(6) << right << fixed << setprecision(2) << i << endl;
         incrementTime(time);
+        if (i > high){
+            high = i;
+        } else if (i < low){
+            low = i;
+        }
+        total += i;
     }
 
+    // calculate statistics of stock
+    change = price[numDataPoints -1] - price[0];
+    average = total / numDataPoints;
+    sort(price.begin(), price.end()); // why reinvent the wheel?
+    median = numDataPoints % 2 == 0 ? (price[(numDataPoints / 2) - 1] + price[(numDataPoints / 2)]) / 2.0
+                                    : price[round(numDataPoints / 2)];
 
+    // display stock statistics
+    cout << endl << setw(12) << left << "CHANGE:" << "$" << fixed << setprecision(2) << setw(6) << right << change << endl;
+    cout << setw(12) << left << "HIGH:" << "$" << fixed << setprecision(2) << setw(6) << right << high << endl;
+    cout << setw(12) << left << "LOW:" << "$" << fixed << setprecision(2) << setw(6) << right << low << endl;
+    cout << setw(12) << left << "AVERAGE:" << "$" << fixed << setprecision(2) << setw(6) << right << average << endl;
+    cout << setw(12) << left << "MEDIAN:" << "$" << fixed << setprecision(2) << setw(6) << right << median << endl;
 
 
 }
